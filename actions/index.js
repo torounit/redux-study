f07e81fce518ab2554602faf17fbc4d8fe50e66e
Ryosuke Goto <ryosuke@direcsio.jp>
2016-06-12 22:03:17 +0900

    Actions are implemented.

--- actions/index.js
+++ actions/index.js
@@ -1,1 +1,5 @@
+import * as types from '../constants/ActionTypes.js'
 
+export const addNote = (text) => ({type: types.ADD_NOTE, text})
+export const deleteNote = (id) => ({type: types.DELETE_NOTE, id: id})
+export const editNote = (id, text) => ({type: types.EDIT_NOTE, id: id, text: text})
