3fc1cbceed2d46495783eeb6a6197acfcf4b6388
Ryosuke Goto <ryosuke@direcsio.jp>
2016-06-12 21:43:28 +0900

    Updated tests to use action types constants.

--- test/actions/notes.spec.js
+++ test/actions/notes.spec.js
@@ -1,25 +1,25 @@
 import expect from 'expect'
 import * as actions from '../../actions'
+import * as types from '../../constants/ActionTypes.js'
 
 describe('note actions', () => {
   it('addNote should create ADD_NOTE action', () => {
     expect(actions.addNote('Use Redux')).toEqual({
-      type: 'ADD_NOTE',
-      id: 0,
+      type: types.ADD_NOTE,
       text: 'Use Redux'
     })
   })
 
   it('deleteNote should create DELETE_NOTE action', () => {
     expect(actions.deleteNote(1)).toEqual({
-      type: 'DELETE_NOTE',
+      type: types.DELETE_NOTE,
       id: 1
     })
   })
 
   it('editNote shoud create EDIT_NOTE action', () => {
     expect(actions.editNote(1, 'Use Redux everywhere')).toEqual({
-      type: 'EDIT_NOTE',
+      type: types.EDIT_NOTE,
       id: 1,
       text: 'Use Redux everywhere'
     })
--- test/reducers/notes.spec.js
+++ test/reducers/notes.spec.js
@@ -1,5 +1,6 @@
 import expect from 'expect'
 import notes from '../../reducers/notes.js'
+import * as types from '../../constants/ActionTypes.js'
 
 describe('notes reducer', () => {
   it('should handle initial state', () => {
@@ -11,7 +12,7 @@ describe('notes reducer', () => {
   it('should handle ADD_NOTE', () => {
     expect(
       notes([], {
-        type: 'ADD_NOTES',
+        type: types.ADD_NOTES,
         text: 'Run the test.',
         id: 0
       })
@@ -29,7 +30,7 @@ describe('notes reducer', () => {
           id: 0
         }
       ], {
-        type: 'ADD_NOTES',
+        type: types.ADD_NOTES,
         text: 'Use Redux',
         id: 1
       })
@@ -55,7 +56,7 @@ describe('notes reducer', () => {
           id: 1
         }
       ], {
-        type: 'ADD_NOTES',
+        type: types.ADD_NOTES,
         text: 'Fix the test.',
         id: 2
       }).toEqual([
@@ -86,7 +87,7 @@ describe('notes reducer', () => {
             id: 1
           }
         ], {
-          type: 'DELETE_NOTE',
+          type: types.DELETE_NOTE,
           id: 1
         }).toEqual([
           {
@@ -109,7 +110,7 @@ describe('notes reducer', () => {
         id: 1
       }
     ], {
-      type: 'EDIT_NOTE',
+      type: types.EDIT_NOTE,
       id: 0,
       text: 'Fix the test.'
     }).toEqual([
