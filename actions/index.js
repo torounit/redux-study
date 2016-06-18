28cc413afb4a052dbc39e6535da62291597f8258
Ryosuke Goto <ryosuke@direcsio.jp>
2016-06-18 22:43:45 +0900

    Now script shows coverage correctly.

--- actions/index.js
+++ actions/index.js
@@ -3,4 +3,4 @@ import { v4 } from 'node-uuid'
 
 export const addNote = (text) => ({ type: types.ADD_NOTE, text: text, id:v4() })
 export const deleteNote = (id) => ({ type: types.DELETE_NOTE, id: id })
-export const editNote = (id, text) => ({type: types.EDIT_NOTE, id: id, text: text})
+export const editNote = (id, text) => ({ type: types.EDIT_NOTE, id: id, text: text })
